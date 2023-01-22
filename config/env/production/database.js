5659d9ef3cf04427379516f41d2db4691c8422da
Adit Firdaus <aditfirdaus.hiklik@gmail.com>
2023-01-22 17:14:24 +0700

    commit message

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,15 @@
 // path: ./config/database.js
-const parse = require("pg-connection-string").parse;
-
-const { host, port, database, user, password } = parse(
-    process.env.DATABASE_URL
-);
-
 module.exports = ({ env }) => ({
     connection: {
         client: 'postgres',
         connection: {
-            host,
-            port,
-            database,
-            user,
-            password,
+            host: env('DATABASE_HOST'),
+            port: env.int('DATABASE_PORT'),
+            database: env('DATABASE_NAME'),
+            user: env('DATABASE_USERNAME'),
+            password: env('DATABASE_PASSWORD'),
             ssl: {
-                rejectUnauthorized: false,
+                ca: env('DATABASE_CA', ''),
             },
         },
         debug: false,
