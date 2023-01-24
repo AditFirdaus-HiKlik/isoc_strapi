395f6a904b96d94d0d8e86a7de353fd754aa06e9
Adit Firdaus <aditfirdaus.hiklik@gmail.com>
2023-01-24 15:47:02 +0700

    Add Nodemailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,21 @@
 module.exports = ({ env }) => ({
+    email: {
+        config: {
+            provider: 'nodemailer',
+            providerOptions: {
+                host: env('SMTP_HOST', 'smtp.example.com'),
+                port: env('SMTP_PORT', 587),
+                auth: {
+                    user: env('SMTP_USERNAME'),
+                    pass: env('SMTP_PASSWORD'),
+                },
+            },
+            settings: {
+                defaultFrom: 'noreply@isoc.co.id',
+                defaultReplyTo: 'noreply@isoc.co.id',
+            },
+        },
+    },
     upload: {
         config: {
             provider: 'aws-s3',
